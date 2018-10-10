812ec8d269b72536b31ae352e9d29be04756b631
Suneha Banu J <sunehabanuj@gmail.com>
2018-10-10 17:33:10 +0530

    Kalam-online-shop-app

--- blog/urls.py
+++ blog/urls.py
@@ -24,10 +24,7 @@ urlpatterns = [
     url(r'^login/author/(?P<pk>\w+)/$',entry_views.author,name='author'),
     url(r'^login/uploader/(?P<pk>\w+)/$',entry_views.uploader,name='uploader'),
     url(r'^password_reset/$', auth_views.password_reset,{'template_name': 'password_reset_form.html'},name='password_reset'), 
-    url(r'^password_reset/done/$', auth_views.password_reset_done,{'template_name': 'password_reset_done.html'},name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',auth_views.password_reset_confirm,
-    {'template_name': 'password_reset_confirm.html'},name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete,{'template_name': 'password_reset_complete.html'},name='password_reset_complete'),    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
